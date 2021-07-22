fabc07baed5e081b663b51c060dba55fa0601503
2h5i <dev.2h5i@gmail.com>
2021-07-23 00:05:46 +0900

    임시

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -9,12 +9,16 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
   @Get()
-  getAll() {
-    return 'This will return all movies';
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
   @Get('/search') // search 가 :id밑에 있으면 id로 인식되기때문에 :id 된 부분들 위로 올려야된다.
@@ -23,18 +27,18 @@ export class MoviesController {
   }
 
   @Get('/:id') // id ㅇㅘ @parma(''id)는 같아야한다.
-  getOne(@Param('id') movieId: string) {
-    return `This will return one movie with the id:${movieId}`;
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
   }
+
   @Post()
   create(@Body() movieData) {
-    console.log(movieData);
-    return movieData;
+    return this.moviesService.create(movieData);
   }
 
   @Delete('/:id')
   remove(@Param('id') movieId: string) {
-    return `This will delete a movie with the id : ${movieId}`;
+    this.moviesService.deleteOne(movieId);
   }
 
   //   @Put() //put은 모든것을 업데이트한다
